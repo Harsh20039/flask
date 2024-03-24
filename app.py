305263ff320de961ce963a7e235c9165636776aa
Harsh20039 <115636119+Harsh20039@users.noreply.github.com>
2024-03-24 17:25:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def simple():
-    return "Hello, Azure Web Apps!"
+    return "hello world"
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
     
